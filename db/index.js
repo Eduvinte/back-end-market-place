3edd9e322bdba5a30f76a52215247eb8930eb2fa
Eduardo Godinho <119964071+EduardoGodinhoerpmagnus@users.noreply.github.com>
2023-07-31 00:15:40 -0400

    fix

--- db/index.js
+++ db/index.js
@@ -1,18 +1,20 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const DB_HOST = process.env.DB_HOST;
-const DB_USER = process.env.DB_USER;
-const DB_PORT = process.env.DB_PORT;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_NAME = process.env.DB_NAME;
+const DB_DATABASE = process.env.DB_DATABASE 
+const DB_HOST = process.env.DB_HOST 
+const DB_PASSWORD = process.env.DB_PASSWORD 
+const DB_PORT = process.env.DB_PORT 
+const DB_USER = process.env.DB_USER
+
+
 
 const pool = new Pool({
+    DB_DATABASE,
     DB_HOST,
-    DB_USER,
-    DB_PORT,
     DB_PASSWORD,
-    DB_NAME
+    DB_PORT,
+    DB_USER
 })
 
 export default pool
