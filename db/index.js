7a2a9af8516ce0047ee528b9253ff3da32b5b08b
Eduardo Godinho <119964071+EduardoGodinhoerpmagnus@users.noreply.github.com>
2023-07-30 23:38:22 -0400

    i

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const DB_HOST = process.env.DB_HOST || 'localhost'
-const DB_USER = process.env.DB_USER || 'postgres'
-const DB_PORT = process.env.DB_PORT || '5432'
-const DB_PASSWORD = process.env.DB_PASSWORD || '689101101024Edu'
-const DB_NAME = process.env.DB_NAME || 'marketplace'
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PORT = process.env.DB_PORT;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
 
 const pool = new Pool({
     DB_HOST,
