33d13e9e873fbd8ac211135a2af7f3c258be200c
Eduardo Godinho <119964071+EduardoGodinhoerpmagnus@users.noreply.github.com>
2023-07-30 23:24:55 -0400

    fix the varivels

--- db/index.js
+++ db/index.js
@@ -8,10 +8,11 @@ const DB_PASSWORD = process.env.DB_PASSWORD || '689101101024Edu'
 const DB_NAME = process.env.DB_NAME || 'marketplace'
 
 const pool = new Pool({
-    database:'marketplace',
-    user: 'postgres',
-    password: '689101101024Edu',
-    port: 5432,
+    DB_HOST,
+    DB_USER,
+    DB_PORT,
+    DB_PASSWORD,
+    DB_NAME
 })
 
 export default pool
