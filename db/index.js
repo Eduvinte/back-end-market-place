f5e124f2c90ad855c3116b1e8c2ad565777b3b15
Eduardo Godinho <119964071+EduardoGodinhoerpmagnus@users.noreply.github.com>
2023-07-31 01:01:09 -0400

    fix

--- db/index.js
+++ db/index.js
@@ -1,20 +1,12 @@
 import pkg from 'pg';
+
 const { Pool } = pkg;
 
-const DB_DATABASE = process.env.DB_DATABASE 
-const DB_HOST = process.env.DB_HOST 
-const DB_PASSWORD = process.env.DB_PASSWORD 
-const DB_PORT = process.env.DB_PORT 
-const DB_USER = process.env.DB_USER
-
-
-
 const pool = new Pool({
-    DB_DATABASE,
-    DB_HOST,
-    DB_PASSWORD,
-    DB_PORT,
-    DB_USER
+   host: 'dpg-cj3jn6diuie55plej2c0-a',
+   port: 5432,
+   user: 'postgress',
+   password: 'iX8gdxeYlo9kDqJ1gAHK5hlODBnXFe3l'
 })
 
 export default pool
