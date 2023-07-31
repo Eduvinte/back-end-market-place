2453ddda958fdcdd7a2004629c8ffa4f5cf0e30d
Eduardo Godinho <119964071+EduardoGodinhoerpmagnus@users.noreply.github.com>
2023-07-31 01:02:18 -0400

    fix

--- db/index.js
+++ db/index.js
@@ -3,10 +3,11 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-   host: 'dpg-cj3jn6diuie55plej2c0-a',
-   port: 5432,
-   user: 'postgress',
-   password: 'iX8gdxeYlo9kDqJ1gAHK5hlODBnXFe3l'
+    database: 'marketplace',
+    host: 'dpg-cj3jn6diuie55plej2c0-a',
+    port: 5432,
+    user: 'postgress',
+    password: 'iX8gdxeYlo9kDqJ1gAHK5hlODBnXFe3l'
 })
 
 export default pool
